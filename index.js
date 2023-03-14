92b80375dc555aa62d2b537ea84671bac96e12e9
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-03-15 01:01:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42; 
+    return Math.abs(pickupLocation - hqLocation);
+  }
+
+  function distanceFromHqInFeet(pickupLocation) {
+    const blockLength = 264; 
+    const distanceInBlocks = distanceFromHqInBlocks(pickupLocation);
+    const distanceInFeet = distanceInBlocks * blockLength; 
+    return distanceInFeet; 
+  }
+  function distanceTravelledInFeet(start, destination) {
+    const blockLength = 264; 
+    const distanceInBlocks = Math.abs(destination - start);
+    const distanceInFeet = distanceInBlocks * blockLength; 
+    return distanceInFeet;
+  }
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start,destination);
+    let fare;
+
+    if (distanceInFeet <= 400) {
+            fare = 0;
+    }
+        else if (distanceInFeet > 400 && distanceInFeet <= 2000){
+            fare = (distanceInFeet- 400) * 0.02
+    }
+        else if (distanceInFeet > 2000 && distanceInFeet <= 2500){
+            fare = 25;
+    }
+        else if (distanceInFeet > 2500){
+            fare = 'cannot travel that far';
+    }
+    
+    
+    return fare
+  }
+
+
